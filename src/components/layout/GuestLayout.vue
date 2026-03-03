e5f10b8b5e7042ceee78cf368692066424ab1780
Jérémie Litzler <jeremie.litzler@gmail.com>
2026-03-03 22:07:25 +0100

    fix(layout): widen the layout

--- src/components/layout/GuestLayout.vue
+++ src/components/layout/GuestLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-guest flex justify-center items-center mx-auto max-w-md">
+  <div class="layout-guest flex justify-center items-center mx-auto max-w-2xl">
     <slot></slot>
   </div>
 </template>
